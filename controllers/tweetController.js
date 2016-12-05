6f4c45f4946539002adf8f9145e2a27cc8d2f55f
Rob Romaniw <robertromaniw@gmail.com>
2016-12-05 10:33:23 -0500

    added Entity Pull analysis

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -20,12 +20,14 @@ var tweetController = {
       })
       searchText.push(text)
       res.json(tweets)
-      callMonkeyLearn(searchText)
+      callSentimentAnalysis(searchText)
+      callEntityPull(searchText)
+      callKeywordExtraction(searchText)
     })
   }
 }
 
-function callMonkeyLearn(searchText){
+function callSentimentAnalysis(searchText){
   var MonkeyLearn = require('monkeylearn');
   var ml = new MonkeyLearn('e5058891ff5968487d756aa9a229e9993325b011');
   var module_id = 'cl_mcHq5Mxu';
@@ -35,14 +37,41 @@ function callMonkeyLearn(searchText){
 // ==============
 // ACTUAL API CALL
 // ==============
+//
+    var p = ml.classifiers.classify(module_id, text_list, true);
 
-    // var p = ml.classifiers.classify(module_id, text_list, true);
-    //
-    // p.then(function (res) {
-    //   console.log(res.result);
-    // });
-
+    p.then(function (res) {
+      console.log(res.result);
+    });
+//
 // ==== end of call ======
+}
+
+function callEntityPull(searchText){
+  var MonkeyLearn = require('monkeylearn');
+
+  var ml = new MonkeyLearn('dd6214d9b29784d3acc6bdc1b3f014731223623e');
+  var module_id = 'ex_isnnZRbS';
+  var text_list = searchText;
+  var p = ml.extractors.extract(module_id, text_list);
+  p.then(function (res) {
+      console.log(res.result);
+  });
+
+
+}
+
+function callKeywordExtraction(searchText){
+  var MonkeyLearn = require('monkeylearn');
+
+  var ml = new MonkeyLearn('dd6214d9b29784d3acc6bdc1b3f014731223623e');
+  var module_id = 'ex_y7BPYzNG';
+  var text_list = searchText;
+  var p = ml.extractors.extract(module_id, text_list);
+  p.then(function (res) {
+      console.log(res.result);
+  });
+
 }
 
 module.exports = tweetController;
