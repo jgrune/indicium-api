7b21d616878cbfc8e8234562e770e06181b94c07
Jonathan Grunewald <jonathan.grunewald1@gmail.com>
2017-01-06 12:59:20 -0500

    removes gitignore

---
+++ keys.js
@@ -0,0 +1,16 @@
+var keys =
+{
+ twitter:
+ {
+   consumer_key:         'YdqghNobO9R8xcx6V7XmCOetX',
+   consumer_secret:      'voNn6hBrm1bWfQKBx33xsRKU5ykaCrAOaeGW06p1x6l3FmRZy6',
+   access_token:         '704384048160870400-6syiHh5XNyoRhu47DQciB9sw5URA4zh',
+   access_token_secret:  'fIwlID9zUVpjLIgSJGL91JyPPEET4oftigxAfHnZZp108'
+
+ },
+ // Rob's Key
+   watson: 'c6eb5c688679c61f0a2a06562145542ab6d89687'
+}
+
+
+module.exports = keys;
